0254b0ff3799add83e72b004236b50b63ebb58f8
VIVEK PATIL <119790479+vivekpatil07@users.noreply.github.com>
2024-01-17 14:00:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", {
+    strings: ["WEB DEVELOPER,", "PYTHON DEVELOPER,","JAVA DEVELOPER,","ROR DEVELOPER,"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+function reload() {
+    location.reload();
+}
+
+
+
+
